b34f420b90d7eb58ad59be816cff505c1c143748
Tonmoy Das <65626663+tonmoy197@users.noreply.github.com>
2024-03-21 22:51:17 +0600

    Time: 4 ms (32.07%), Space: 9.3 MB (29.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,8 +2,11 @@ class Solution {
 public:
 
     int solve(vector<int>& nums, vector<int>& dp, int i){
+        // base case 
         if(i >= nums.size() ) return 0;
+        
         if(dp[i] == -1){
+            // taking current element and 2 next element , and taking next element
             dp[i] = max( solve(nums, dp, i + 1), nums[i] + solve(nums, dp, i + 2));
         }
 
