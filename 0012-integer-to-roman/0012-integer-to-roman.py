67934943812d5ddee53f7827bb6477c8bf6da3ec
Tonmoy Das <65626663+tonmoy197@users.noreply.github.com>
2024-06-23 11:31:25 +0600

    Time: 37 ms (30.94%), Space: 11.5 MB (91.14%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def intToRoman(self, num):
+        ones = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+        tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
+        hrns = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
+        ths = ["","M","MM","MMM"];
+
+        ans = ths[num / 1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10]
+        return ans
